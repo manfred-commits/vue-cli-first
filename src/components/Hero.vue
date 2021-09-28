fc3d14952c0f3e8ec5aa3f5346f979cc07a140ea
manfred-commits <manfredipiraino@gmail.com>
2021-09-28 16:59:43 +0200

    completed bonus "with img taken from assets"

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="container-img">
 
-  <img class="img-formatting" src="https://cdn.vox-cdn.com/thumbor/rw5wQeIgykyM4wlKhSNKTs6N-io=/0x0:1250x694/1200x800/filters:focal(525x247:725x447)/cdn.vox-cdn.com/uploads/chorus_image/image/69049871/Big_Chungus.0.png" alt="jumbotron img">
+  <img class="img-formatting" src="../assets/Big_chungus.png" alt="jumbotron img">
 
 </div>
 </template>
